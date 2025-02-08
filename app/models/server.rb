6eb42687699fbb460e9164504646deb1c1918057
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-02-08 20:14:31 +0700

    first commit

---
+++ app/models/server.rb
@@ -0,0 +1,6 @@
+class Server < ApplicationRecord
+  belongs_to :owner, class_name: "User"
+  has_many :channels, class_name: "Server::Channel", foreign_key: "server_id"
+  has_many :invite_links, class_name: "Server::InviteLink"
+  has_many :memberships, class_name: "Server::Membership"
+end
